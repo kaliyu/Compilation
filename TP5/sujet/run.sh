ada30fe201154f0bf9d69fea878bc073e2f3e7b1
Roy Tom <tom.roy@sfr.fr>
2025-03-17 09:54:50 +0100

    TP5

---
+++ TP5/sujet/run.sh
@@ -0,0 +1,6 @@
+VERSION=4.13.2
+main="main.Main"
+path="/home/o22100171@campus.univ-orleans.fr/Bureau/compil/Compilation/TP5/sujet/antlr-"$VERSION"-complete.jar"
+export CLASSPATH=$CLASSPATH:$path
+
+java $main
